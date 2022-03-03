907aece7cc8c96ad1beb5769597f8d5aa74fafa9
PabloAH1 <pablo.aguilar-horrillo@capgemini.com>
2022-03-03 09:07:49 +0100

    Empezando

---
+++ src/test/java/com/formacion/app/ApiRestRrhhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacion.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestRrhhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
